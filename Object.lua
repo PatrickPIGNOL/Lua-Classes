c71f464f4fa7160e71bee814850e81fcf60aee67
Patrick PIGNOL <patrick.pignol@gmail.com>
2021-02-09 01:49:32 +0100

    2021-02-09

--- Object.lua
+++ Object.lua
@@ -1,6 +1,6 @@
 -- classe mère
 Object = {} -- metatable de Shape
---setmetatable(Object, {__index = Object}) -- un Object à la metatable d'un Oject
+setmetatable(Object, {__index = Object}) -- un Object à la metatable d'un Oject
 function Object:new(pX, pY)-- constructeur de Object
     self = {} --self est un nouvel objet de type table ( {} )
     setmetatable(self, {__index = Object}) --self est un Object
